b8234b857c3764dd7851998cd03e452c1e0600a1
dennis <dennis@dennis.com>
2018-06-05 00:06:11 +0800

    fix popup

--- src/somuchmeeting-ui/src/view/index.vue
+++ src/somuchmeeting-ui/src/view/index.vue
@@ -40,45 +40,40 @@
         </el-table>
       </div>
     </el-card>
-    <el-dialog title="发起/修改会议" :visible.sync="formInline.dialogTableVisible" :before-close="handleClose">
+    <el-dialog title="发起/修改会议" :visible.sync="formInline.dialogTableVisible" width="30%" :before-close="handleClose">
       <el-form ref="form" :model="meetingInfo" label-width="80px">
-        <el-form-item label="活动名称">
-          <el-input v-model="meetingInfo.name"></el-input>
+        <el-form-item label="会议时间">
+          <div class="block">
+            <el-date-picker
+              v-model="formInline.value6"
+              type="datetimerange"
+              start-placeholder="开始时间"
+              end-placeholder="结束时间">
+            </el-date-picker>
+          </div>
         </el-form-item>
-        <el-form-item label="活动区域">
-          <el-select v-model="meetingInfo.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
+        <el-form-item label="会议室">
+          <el-select v-model="value8" filterable placeholder="请选择">
+          <el-option
+            v-for="item in formInline.options"
+            :key="item.value"
+            width="400px"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
         </el-form-item>
-        <el-form-item label="活动时间">
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="meetingInfo.date1" style="width: 100%;"></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="meetingInfo.date2" style="width: 100%;"></el-time-picker>
-          </el-col>
+        <el-form-item label="参会人数">
+          <el-input type="number" v-model="meetingInfo.name"></el-input>
         </el-form-item>
-        <el-form-item label="即时配送">
-          <el-switch v-model="meetingInfo.delivery"></el-switch>
-        </el-form-item>
-        <el-form-item label="活动性质">
-          <el-checkbox-group v-model="meetingInfo.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-            <el-checkbox label="地推活动" name="type"></el-checkbox>
-            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="特殊资源">
-          <el-radio-group v-model="meetingInfo.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="活动形式">
-          <el-input type="textarea" v-model="meetingInfo.desc"></el-input>
+        <el-form-item label="会议主题">
+          <el-input
+            type="textarea"
+            :rows="2"
+            width="400px"
+            placeholder="请注明会议主题"
+            v-model="meetingInfo.name">
+          </el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="formInline.dialogTableVisible = false">立即创建</el-button>
@@ -126,6 +121,8 @@
           desc: ''
         },
         formInline: {
+          value6:[],
+          value8:[],
           dialogTableVisible: false,
           pickerOptions2: {
             shortcuts: [{
--- src/somuchmeeting-ui/src/view/infrastructure.vue
+++ src/somuchmeeting-ui/src/view/infrastructure.vue
@@ -40,45 +40,30 @@
         </el-table>
       </div>
     </el-card>
-    <el-dialog title="发起/修改会议" :visible.sync="formInline.dialogTableVisible" :before-close="handleClose">
+    <el-dialog title="添加/编辑设备" :visible.sync="formInline.dialogTableVisible" width="30%" :before-close="handleClose">
       <el-form ref="form" :model="meetingInfo" label-width="80px">
-        <el-form-item label="活动名称">
-          <el-input v-model="meetingInfo.name"></el-input>
+        <el-form-item label="设备名称">
+          <el-input v-model="meetingInfo.name" clearable></el-input>
         </el-form-item>
-        <el-form-item label="活动区域">
-          <el-select v-model="meetingInfo.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
+        <el-form-item label="设备品牌">
+          <el-input v-model="meetingInfo.name" clearable></el-input>
         </el-form-item>
-        <el-form-item label="活动时间">
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="meetingInfo.date1" style="width: 100%;"></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="meetingInfo.date2" style="width: 100%;"></el-time-picker>
-          </el-col>
+        <el-form-item label="设备型号">
+          <el-input v-model="meetingInfo.name" clearable></el-input>
         </el-form-item>
-        <el-form-item label="即时配送">
-          <el-switch v-model="meetingInfo.delivery"></el-switch>
+        <el-form-item label="缩略图">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
         </el-form-item>
-        <el-form-item label="活动性质">
-          <el-checkbox-group v-model="meetingInfo.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-            <el-checkbox label="地推活动" name="type"></el-checkbox>
-            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="特殊资源">
-          <el-radio-group v-model="meetingInfo.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="活动形式">
-          <el-input type="textarea" v-model="meetingInfo.desc"></el-input>
+        <el-form-item label="当前库存">
+          <el-input type="number" v-model="meetingInfo.name" clearable></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="formInline.dialogTableVisible = false">立即创建</el-button>
@@ -188,7 +173,8 @@ export default {
           brand:'TCL',
           count: 8,
           create_time: '2016-05-05 20:06'
-        }]
+        }],
+        imageUrl: ''
       }
     },
     methods: {
@@ -197,6 +183,21 @@ export default {
       },
       handleClose(done) {
 
+      },
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
     }
 }
